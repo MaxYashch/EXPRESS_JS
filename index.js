a91e972eb37585337d8f9ce9b6f4e3bdb7293b1d
MaxYashch <maxyashch@gmail.com>
2022-09-26 18:49:39 +0300

    added router

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
 
 const port = 8000;
 const app = express();
 
 app.get('/hello', (req, res) => {
     res.send('HI MAN');
-})
+});
+
+app.use('/users', userRouter);
 
 app.listen(port, () => {
     console.log(`Server has been running on http://localhost:${port}`);
-})
+});

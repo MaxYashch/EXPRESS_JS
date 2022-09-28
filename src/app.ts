f3c1961864909de03a6e24ca874f187da9b2c620
MaxYashch <maxyashch@gmail.com>
2022-09-28 13:40:16 +0300

    added class LoggerService

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, { Express } from 'express';
 import { Server } from 'http';
 import { userRouter } from './users/users';
+import { LoggerService } from './logger/logger_service';
 
 export class App {
     app: Express;
     server: Server;
     port: number;
+    logger: LoggerService;
 
-    constructor() {
+    constructor(logger: LoggerService) {
         this.app = express();
         this.port = 8000;
+        this.logger = logger;
     }
 
     useRoutes() {
@@ -19,7 +22,7 @@ export class App {
     public async init() {
         this.useRoutes();
         this.server = this.app.listen(this.port);
-        console.log(`Server has established on http://localhost:${this.port}`);
+        this.logger.log(`Server has established on http://localhost:${this.port}`);
         
     }
 }
